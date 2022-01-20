e0d653244e07c39072bfb8b9de6688ce65927d23
Robin91862 <95881419+Robin91862@users.noreply.github.com>
2022-01-20 12:23:45 +0100

    Add files via upload

---
+++ System firmy kurierskiej DPD.py
@@ -0,0 +1,64 @@
+import sys
+
+print("Witaj w systemie DPD")
+
+print("Co chcesz zrobić?")
+
+print("1 - Wyślij paczkę")
+
+print("2 - Zaloguj się")
+
+wybór = input()
+
+
+if wybór == '1':
+    print("Podaj dane paczki")
+
+    input("Adres: ")
+
+    input("Kod pocztowy: ")
+
+    input("Miasto: ")
+
+    input("Kod BLIK: ")
+    
+    skończenieWpisywaniaAdresu = input("Wciśnij Enter")
+
+    if skończenieWpisywaniaAdresu == '':
+        print("Ok, twoja paczka zostanie wkrótce nadana! Dziękujemy i zapraszamy ponownie!")
+        print()
+        print("Co chcesz zrobić?")
+        print("0 - Wyjdź z aplikacji")
+        zakończenie = input()
+        if zakończenie == '0':
+            sys.exit()
+
+if wybór == '2':
+    print("Wprowadź dane!")
+    login = input("Wprowadź login: ")
+    hasło = input("Wprowadź hasło: ")
+    if hasło == 'password' and login == 'Robin91862':
+        print("Zalogowano!")
+    else:print("Wprowadzono błędne dane!")
+    print("Co chcesz zrobić?")
+    print("1 - Wyślij za darmo")
+    wyślij = input()
+    if wyślij == '1':
+        print("Podaj dane paczki")
+
+        input("Adres: ")
+
+        input("Kod pocztowy: ")
+
+        input("Miasto: ")
+
+        skończenieWpisywaniaAdresu2 = input("Wciśnij Enter")
+
+        if skończenieWpisywaniaAdresu2 == '':
+            print("Ok, twoja paczka zostanie wkrótce nadana! Dziękujemy i zapraszamy ponownie!")
+            print()
+            print("Co chcesz zrobić?")
+            print("0 - Wyjdź z aplikacji")
+            zakończenie2 = input()
+            if zakończenie2 == '0':
+                sys.exit()
